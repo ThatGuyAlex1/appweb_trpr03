3af65301960ad015f33a95522cc647f1afe3e1b4
Céleste <deucefan33@gmail.com>
2024-05-07 16:23:31 -0400

    pull commit

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -24,16 +24,10 @@ function logout() {
         <RouterLink
           class="nav-link"
           :class="{ active: $route.name == 'Home' }"
+          v-if="isLoggedIn"
           :to="{ name: 'Home' }"
         >
           Accueil
-        </RouterLink>
-        <RouterLink
-          class="nav-link"
-          :class="{ active: $route.name == 'About' }"
-          :to="{ name: 'About' }"
-        >
-          À propos
         </RouterLink>
 
         <!-- La page Profile n'est accessible que si l'utilisateur est connecté (v-if). Voir la propriété calculée isLoggedIn() qui retourne la valeur de la propriété isLoggedIn du store. -->
@@ -48,9 +42,7 @@ function logout() {
       </div>
       <div class="d-flex">
         <div class="navbar-nav ml-auto">
-          <a class="nav-link" @click="logout" v-if="isLoggedIn" href="#">
-            Se déconnecter
-          </a>
+          <a class="nav-link" @click="logout" v-if="isLoggedIn" href="#"> Se déconnecter </a>
           <RouterLink
             v-else
             class="nav-link"
