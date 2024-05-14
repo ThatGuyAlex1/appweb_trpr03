a1827672b3c97f0fe8ef14fe9f56cf5224143017
William Villeneuve <71040567+WilliamVilleneuve16@users.noreply.github.com>
2024-05-14 14:03:25 -0400

    Esquisse de la page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
+import CreateQuestionTypeForm from '../components/CreateQuestionTypeFormComponent.vue'
+import QuestionForm from '@/components/QuestionFormComponent.vue';
+import QuestionList from '../components/QuestionListComponent.vue'; 
+
+import { computed } from 'vue'
+import { useAuthStore } from '../stores/authStore'
+import { useProfileStore } from '../stores/profileStore'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const profileStore = useProfileStore()
+const router = useRouter()
+
+const isLoggedIn = computed(() => authStore.isLoggedIn)
+//Meilleure syntaxe donnée par Jimmy ! Merci ;)
+const isTeacher = computed(() => profileStore.role === 'teacher')
+
+function logout() {
+  authStore.logout()
+  profileStore.logout()
+  router.push({
+    name: 'Login'
+  })
+}
 </script>
 
 <template>
   <div class="home">
     <h1>Accueil</h1>
-    <p>La page "Accueil" est accessible par tout le monde.</p>
+    <p v-if="isTeacher">La liste des questions des vos étudiants. Pour ajouter une nouvelle catégorie de question, rempliser le formulaire a droite</p>
+    <p v-else>La liste des questions en attentes. Pour "lever la main", remplissez le formulaire a droite</p>
+    <div class="row">
+      <div class="col-md-7">
+        <QuestionList />
+      </div>
+
+      <div class="col-md-5">
+        <CreateQuestionTypeForm v-if="isTeacher" />
+        <QuestionForm v-else />
+      </div>
+    </div>
   </div>
 </template>
 
