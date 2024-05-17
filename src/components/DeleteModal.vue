9477667bf0298336868a7e3a099b7b29eabb47f7
Céleste <deucefan33@gmail.com>
2024-05-17 16:01:34 -0400

    modals ok classManager

---
+++ src/components/DeleteModal.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { Modal } from 'bootstrap'
+import { ref, watch, onMounted } from 'vue'
+
+// Définir les props avec des types
+const props = defineProps({
+  trigger: Number,
+  title: String,
+  body: String,
+  dismissButton: String
+})
+
+// Utiliser ref pour créer une référence réactive à null qui sera utilisée pour stocker l'instance de la modal
+const modal = ref<Modal | null>(null)
+
+// Le cycle de vie onMounted remplace mounted de l'API Options.
+// Il est exécuté après que le composant soit monté dans le DOM.
+onMounted(() => {
+  if (document.querySelector('#delete-modal')) {
+    modal.value = new Modal('#delete-modal')
+  }
+})
+
+// Utiliser watch pour observer les changements de la prop 'trigger' et afficher la modal en conséquence
+watch(
+  () => props.trigger,
+  (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      modal.value?.show()
+    }
+  }
+)
+</script>
+
+<!-- Ce composant permet d'afficher une fenêtre modale de confirmation. Il est utilisée dans le composant PostDetailView pour confirmer la modifcation d'une publication. C'est Bootstrap qui est utilisé pour l'affichage (https://getbootstrap.com/docs/5.2/components/modal/) de la fenêtre modale. Il existe aussi plusieurs libraires externes qui pourraient être utilisées à la place de Bootstrap, lesquelles nous éviteraient de créer ce composant, mais nous obligeraient à apprendre à utiliser une nouvelle librairie. -->
+<template>
+  <div
+    class="modal fade"
+    id="delete-modal"
+    tabindex="-1"
+    aria-labelledby="delete-modal"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title">{{ title }}</h5>
+        </div>
+        <div class="modal-body">
+          {{ body }}
+        </div>
+        <div class="modal-footer">
+          <button name="confirmer" type="button" data-bs-dismiss="modal" class="btn btn-primary">
+            {{ dismissButton }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/ClassManagerView.vue
+++ src/views/ClassManagerView.vue
@@ -3,59 +3,72 @@ import { ref, onMounted, computed, nextTick } from 'vue'
 import { Field, Form, ErrorMessage, defineRule, validate } from 'vee-validate'
 import { required } from '@vee-validate/rules'
 import { useStudentStore } from '../stores/studentStore'
-import studentForm from '../components/CreateStudentFormComponent.vue'
+import StudentForm from '../components/CreateStudentFormComponent.vue'
 import type User from '../scripts/user'
 import Loading from 'vue-loading-overlay'
+import { useToast } from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-sugar.css'
+import DeleteModal from '../components/DeleteModal.vue'
+import CreateModal from '../components/CreateModal.vue'
 
 const studentStore = useStudentStore()
 
 const students = computed(() => studentStore.students as User[])
 const onError = computed(() => studentStore.onError)
 const isLoading = ref(false)
-const showForm = ref(false)
+const triggerDeleteModal = ref(0)
+const triggerCreateModal = ref(0)
 
 onMounted(async () => {
   isLoading.value = true
   try {
     await studentStore.getStudents()
     if (onError.value) {
-      // Utilisation d'une boîte de dialogue au lieu de 'confirm'
-      confirm("Une erreur s'est produite lors de la récupération des étudiants.")
+      useToast().error(`Erreur avec le service: Erreur lors de la récupération des étudiants`, {
+        duration: 6000
+      })
     }
   } catch (error) {
-    confirm("Erreur critique lors de l'accès au store.")
+    useToast().error(
+      `Erreur avec le service: ${(error as Error).message}. Oups, le backend a lâché !`,
+      { duration: 6000 }
+    )
   } finally {
     isLoading.value = false
   }
 })
 
 async function reloadStudents() {
-  isLoading.value = true
   try {
     await studentStore.getStudents()
   } catch (error) {
-    confirm('Erreur lors du rechargement des étudiants.')
+    useToast().error(
+      `Erreur avec le service: ${(error as Error).message}. Erreur lors du rafraîchissement des étudiants (Tellement sec !).`,
+      { duration: 6000 }
+    )
   } finally {
     isLoading.value = false
   }
 }
 
-async function showStudentForm() {
-  if (showForm.value == true) {
-    showForm.value = false
-  } else if (showForm.value == false) {
-    showForm.value = true
+async function deleteStudent(user: User) {
+  try {
+    isLoading.value = true
+    await studentStore.deleteSpecificStudent(user.id)
+    triggerDeleteModal.value++
+    reloadStudents()
+  } catch (error) {
+    useToast().error(
+      `Erreur avec le service: ${(error as Error).message}. Erreur lors de l'effacement de l'étudiant.`,
+      { duration: 6000 }
+    )
   }
 }
 
-async function deleteStudent(user: User) {
-  try {
-    await studentStore.deleteSpecificStudent(user.id)
-    confirm("L'étudiant a été supprimé avec succes")
-    reloadStudents()
-  } catch (error) {
-    confirm("Une erreur s'est produite lors de l'effacement de l'utilisateur.")
-  }
+function handleNewStudent() {
+  isLoading.value = true
+  reloadStudents()
+  triggerCreateModal.value++
 }
 </script>
 
@@ -64,7 +77,7 @@ async function deleteStudent(user: User) {
     <div v-if="!isLoading">
       <div>
         <h1 class="text-center">Créer un nouvel étudiant</h1>
-        <studentForm @new-student="reloadStudents" />
+        <StudentForm @new-student="handleNewStudent" />
       </div>
       <h1 class="text-center">Liste des étudiants</h1>
       <table class="table">
@@ -86,6 +99,22 @@ async function deleteStudent(user: User) {
         </tbody>
       </table>
     </div>
+
+    <!--Modal de supression d'étudiant-->
+    <DeleteModal
+      :trigger="triggerDeleteModal"
+      title="Succès !"
+      body="L'étudiant a été supprimé avec succès."
+      dismissButton="Ok"
+    />
+    <!--Modal de création d'étudiant-->
+    <CreateModal
+      :trigger="triggerCreateModal"
+      title="Succès !"
+      body="L'étudiant a été crée avec succès."
+      dismissButton="Ok"
+    />
+
     <div class="pt-5 mt-5 d-flex justify-content-center align-items-center">
       <Loading :active="isLoading" />
     </div>
